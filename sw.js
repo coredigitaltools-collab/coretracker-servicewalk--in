51e3ba8a3763c0b23199a606b1df986a1ca6faea
coredigitaltools-collab <coredigital.tools@gmail.com>
2026-04-17 09:31:26 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='ctlite-v1';
-const ASSETS=['./', './index.html', './manifest.json', './icons/icon-192x192.png', './icons/icon-512x512.png',
-  'https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,300;0,400;0,500;1,400&family=DM+Mono:wght@400;500&display=swap',
-  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.umd.js'
-];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(cached=>{if(cached)return cached;return fetch(e.request).then(res=>{if(!res||res.status!==200||res.type==='opaque')return res;const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));return res;}).catch(()=>caches.match('./index.html'));}));});
